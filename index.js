f4825784f33950d673019151ebef29d659610eed
Agn1ss <iggolnik@gmail.com>
2025-09-26 23:36:26 +0300

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function gcd(a, b) {
+    a = BigInt(a);
+    b = BigInt(b);
+    return b === 0n ? a : gcd(b, a % b);
+}
+
+function getLCM(a, b) {
+    a = BigInt(a);
+    b = BigInt(b);
+    return (a * b) / gcd(a, b);
+}
+
+function isNaturalNumber(str) {
+    return /^[1-9]\d*$/.test(str);
+}
+
+app.get('/', (req, res) => {
+    const xStr = req.query.x;
+    const yStr = req.query.y;
+    
+    if (!xStr || !yStr || !isNaturalNumber(xStr) || !isNaturalNumber(yStr)) {
+        return res.send('NaN');
+    }
+    
+    try {
+        const result = getLCM(xStr, yStr);
+        res.send(result.toString());
+    } catch (error) {
+        res.send('NaN');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
